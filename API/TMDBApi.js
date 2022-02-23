30fa7ae1151d9ef1c1bdbc807dd7fbda37beaa19
safae2021 <safaetahere.st@gmail.com>
2022-02-23 22:01:27 +0100

    first commit

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -1,7 +1,8 @@
 
 
 const API_TOKEN ="6af2198ca66343ed8e37d7631e7e5fcf";
-export function getFilmsFromApiWithSearchedText (text) {
+export function getFilmsFromApiWithSearchedText (text) 
+{
     const url = 'https://api.themoviedb.org/3/movie/550?api_key=' + API_TOKEN + '&language=fr&query=' + text
 
     return fetch(url)  
